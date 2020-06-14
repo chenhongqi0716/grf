dfe75814d6916e3ec3e0b8894e01d7e251b9ffbf
chenh <chenhongqi0716@gmail.com>
2020-06-14 14:31:19 -0500

    Update ForestOptions.cpp
    copy from the previous ForestOptions.

--- core/src/forest/ForestOptions.cpp
+++ core/src/forest/ForestOptions.cpp
@@ -76,8 +76,29 @@ ForestOptions::ForestOptions(uint num_trees,
                              uint random_seed,
                              const std::vector<size_t>& sample_clusters,
                              uint samples_per_cluster,
-                             uint blocklength) {
-  // TODO
+                             uint blocklength) :
+    ci_group_size(ci_group_size),
+    sample_fraction(sample_fraction),
+    tree_options(mtry, min_node_size, honesty, honesty_fraction, honesty_prune_leaves, alpha, imbalance_penalty),
+    sampling_options(samples_per_cluster, sample_clusters, blocklength){
+  // Copy from above. Add blocklength in sampling_options().
+  this->num_threads = validate_num_threads(num_threads);
+  
+  // If necessary, round the number of trees up to a multiple of
+  // the confidence interval group size.
+  this->num_trees = num_trees + (num_trees % ci_group_size);
+  
+  if (ci_group_size > 1 && sample_fraction > 0.5) {
+    throw std::runtime_error("When confidence intervals are enabled, the"
+                               " sampling fraction must be less than 0.5.");
+  }
+  
+  if (random_seed != 0) {
+    this->random_seed = random_seed;
+  } else {
+    std::random_device random_device;
+    this->random_seed = random_device();
+  }
 }
 
 uint ForestOptions::get_num_trees() const {
