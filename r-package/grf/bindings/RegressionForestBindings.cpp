52a036fc1fb5bfec7652e9b71143184b3e0268f3
chenh <chenhongqi0716@gmail.com>
2020-06-27 20:13:31 -0500

    Update RegressionForestBindings.cpp

--- r-package/grf/bindings/RegressionForestBindings.cpp
+++ r-package/grf/bindings/RegressionForestBindings.cpp
@@ -63,7 +63,7 @@ Rcpp::List regression_train(Rcpp::NumericMatrix train_matrix,
   ForestOptions options(num_trees, ci_group_size, sample_fraction, mtry, min_node_size, honesty,
                         honesty_fraction, honesty_prune_leaves, alpha, imbalance_penalty, num_threads, seed, clusters, samples_per_cluster);
   if(mbb){
-      options = ForestOptions{;num_trees, ci_group_size, sample_fraction, mtry, min_node_size, honesty,
+      options = ForestOptions{num_trees, ci_group_size, sample_fraction, mtry, min_node_size, honesty,
           honesty_fraction, honesty_prune_leaves, alpha, imbalance_penalty, num_threads, seed, clusters, samples_per_cluster,
           blocklength, blocknum};
   }
